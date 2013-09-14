b3e8c44ad161e15bd9e7d3cd9ef278312995df0f
Damian Boni <damian.boni@vuma.com>
2013-09-14 20:17:24 +0100

    commit from DamoOfTheDigitalbox

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-<<<<<<< HEAD
+//added from DamoOfTheDigitalbox.
 //added from damoBox
-||||||| merged common ancestors
 //test branch traking.
-=======
 //checking commits by another user.
 //test branch traking.
->>>>>>> 90caa31ec953004605fdeb090e8d01e1314bea11
 //master updates to stay.
 phpinfo();
 ?>
