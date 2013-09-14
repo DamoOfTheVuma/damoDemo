1dd16f0808e1bd85b5e845dc4bdff650b2abcd07
Damian Boni <damian.boni@vuma.com>
2013-09-14 18:24:15 +0100

    updates.

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
 //master updates to stay.
-||||||| merged common ancestors
-// new modifications from damoTestBranch, i want damoTestBranch mods to stay in the file desipte whatever master changes happend.
-=======
 phpinfo();
 ?>
